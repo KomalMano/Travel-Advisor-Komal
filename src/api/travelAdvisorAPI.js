ea39238021e6b53791b490248130c0254b12038e
unknown <km9681226@gmail.com>
2022-11-10 11:58:27 +0500

    first commit

---
+++ src/api/travelAdvisorAPI.js
@@ -0,0 +1,41 @@
+/* eslint-disable consistent-return */
+import axios from 'axios';
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_API_KEY,
+        'X-Rapidapi-Host': 'travel-advisor.p.rapidapi.com',
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get('https://forecast9.p.rapidapi.com/', {
+        params: { lat: lat, lon: lng, },
+        headers: {
+          'X-Rapidapi-Key': process.env.REACT_APP_RAPIDAPI_API_KEY,
+          'X-Rapidapi-Host': 'forecast9.p.rapidapi.com',
+        },
+      });
+
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
